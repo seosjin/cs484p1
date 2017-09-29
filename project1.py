e7740412a98d07b61df5cead7203711a7081f73a
meow <seosjin@gmail.com>
2017-09-29 14:30:59 -0400

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,93 @@
+from socket import *
+import os
+import time
+from time import mktime
+from datetime import datetime
+from wsgiref.handlers import format_date_time
+
+serverPort = 11600
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+
+def setHeader(x, f = ""):
+    #I used https://stackoverflow.com/questions/237079/how-to-get-file-creation-modification-date-times-in-python to find the last modified time. West Point, Ny. 27 September 2017.
+    timeMod = "Last-Modified: " + str(format_date_time(mktime(time.localtime(os.path.getmtime(f))))) + "\n"
+    #I used https://stackoverflow.com/questions/225086/rfc-1123-date-representation-in-python for the proper HTTP date-time format. West Point, NY. 27 September 2017.
+    timeNow = "Date: " + str(format_date_time(mktime(datetime.now().timetuple()))) + "\n"
+
+    connection = "Connection: close\n"
+
+    contentType = "Content-Type: "
+    ext = f.split('.')[-1]
+    if ext == "html":
+        contentType += "text/html\n"
+        length = "Content-Length: " + str(len(open(f,'r').read()))
+    elif ext == "css":
+        contentType += "text/css\n"
+        length = "Content-Length: " + str(len(open(f,'r').read()))
+    else:
+        contentType += "image/jpeg\n"
+        length = "Content-Length: " + str(len(open(f,'rb').read()))
+
+    status = "HTTP/1.1 "
+    if x == 200:
+        status += "200 OK\n"
+    elif x == 301:
+        status += "301 Moved Permanently\n"
+    else:
+        status += "404 Not Found\n"
+        contentType = "Content-Type: text/html\n"
+        header = status + timeNow + conenction + contentType + length
+        header += str(len(header))
+        return header
+
+    header = status + timeNow + timeMod + connection + contentType + length
+
+    return header
+
+def validRequest(x, files):
+    if x == '/' or x == "test1.html":
+        return 0
+    if x[1:] == "white.html":
+        return 2
+    if x[1:] == "it105.html":
+        return 11
+    for i in range(0, len(files)):
+        if x[1:] in files[i].replace("\\","/"):
+            return i
+    return -1
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    message = connectionSocket.recv(1024).decode()
+    parsed = message.split('\r\n')
+    files = []
+
+    #https://stackoverflow.com/questions/954504/how-to-get-files-in-a-directory-including-all-subdirectories used to look for files in the subdirectories. West Point, NY. 28 September 2017.
+    for dirpath, dirnames, filenames in os.walk("./proj1files"):
+        for filename in [f for f in filenames]:
+            files.append(os.path.join(dirpath, filename))
+
+    try:
+        iOfFile = validRequest(parsed[0].split(' ')[1], files)
+        if iOfFile == -1:
+            header = setHeader(404)
+            connectionSocket.send((header + "\n\n" + f).encode())
+            conenctionSocket.close()
+            continue
+        if iOfFile == 1 or iOfFile == 3:
+            header = setHeader(301,files[iOfFile])
+        else:
+            header = setHeader(200,files[iOfFile])
+        if "jpg" in files[iOfFile] or "jpeg" in files[iOfFile]:
+            f = open(files[iOfFile],'rb').read()
+            connectionSocket.send((header + "\n\n").encode() + f)
+        else:
+            f = open(files[iOfFile],'r').read()
+            connectionSocket.send((header + "\n\n" + f).encode())
+
+    except:
+        continue
+
+    connectionSocket.close()
